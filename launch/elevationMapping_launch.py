fde775bcdef372ce9b8dba164f4447f0ba6fc1c7
helhest <helhest-robot@fel.cvut.cz>
2025-06-10 14:32:18 +0200

    better launch; configs for sr camera

--- launch/elevationMapping_launch.py
+++ launch/elevationMapping_launch.py
@@ -1,60 +1,60 @@
 import os
 import launch
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from ament_index_python.packages import get_package_share_directory
 
 
-def generate_launch_description():
-
+def launch_setup(context, *args, **kwargs):
     share_dir = get_package_share_directory('elevation_mapping')
     config_dir = os.path.join(share_dir, 'config')
-    xacro_path = os.path.join(config_dir, 'robot.urdf.xacro')
-    list_params = []
-    # for filee in ["robots/ground_truth_demo.yaml","elevation_maps/long_range.yaml","sensor_processors/realsense_d435.yaml","postprocessing/postprocessor_pipeline.yaml"]:
-    for filee in ["robots/marv.yaml","postprocessing/postprocessor_pipeline.yaml"]:
-        list_params.append(os.path.join(config_dir, filee))
-        
-    return launch.LaunchDescription(
-        [
-            # launch_ros.actions.Node(
-            #     package='elevation_mapping',
-            #     executable='pose_publisher_node',
-            #     name='pose_publisher_node',
-            #     output='screen',
-            #     parameters=list_params,
-            # ),
-            launch_ros.actions.Node(
-                package='elevation_mapping',
-                executable='gridmap_to_pointcloud_node',
-                name='gridmap_to_pointcloud_node',
-                output='screen',
-                parameters=list_params,
-            ),
-            launch_ros.actions.Node(
-                package='elevation_mapping',
-                executable='elevation_mapping',
-                name='elevation_mapping',
-                output='screen',
-                parameters=list_params,
-                # arguments=['--ros-args', '--log-level', 'DEBUG']
-            ),
-            #launch_ros.actions.Node(
-            #package='robot_state_publisher',
-            #executable='robot_state_publisher',
-            #name='robot_state_publisher',
-            #output='screen',
-            #parameters=[{
-            #    'robot_description': launch.substitutions.Command(['xacro', ' ', xacro_path])
-            #}]
-        #)
-            # launch_ros.actions.Node(
-            #     package= 'rviz2',
-            #     executable= 'rviz2',
-            #     name= 'rviz',
-            #     arguments= ['--display-config', os.path.join(share_dir, 'rviz2', 'custom_rviz2.rviz')],
-            #     output= 'screen'
-            # )
-        ]
-    )
+    
+    config_file = LaunchConfiguration('config_file').perform(context)
+    map_topic_name = LaunchConfiguration('map_topic_name').perform(context)
+
+    list_params = [
+        os.path.join(config_dir, config_file),
+        os.path.join(config_dir, "postprocessing/postprocessor_pipeline.yaml")
+    ]
+
+    remappings = [
+        ('elevation_map', map_topic_name),
+    ]
+
+    return [
+        Node(
+            package='elevation_mapping',
+            executable='gridmap_to_pointcloud_node',
+            name='gridmap_to_pointcloud_node',
+            output='screen',
+            parameters=list_params,
+            remappings=remappings,
+        ),
+        Node(
+            package='elevation_mapping',
+            executable='elevation_mapping',
+            name='elevation_mapping',
+            output='screen',
+            parameters=list_params,
+            remappings=remappings,
+            # arguments=['--ros-args', '--log-level', 'DEBUG']
+        ),
+    ]
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        DeclareLaunchArgument(
+            'config_file',
+            default_value=os.path.join(
+                get_package_share_directory('elevation_mapping'), 'config'),
+            description='Config file.'
+        ),
+        DeclareLaunchArgument(
+            'map_topic_name',
+            default_value="elevation_map",
+            description='Name of the elevation map topic published by elevation_mapping.'
+        ),
+        OpaqueFunction(function=launch_setup)
+    ])
